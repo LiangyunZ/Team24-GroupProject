e306955a8d8f56d3049e01aa725b3fefed003901
krishg <krishgaglani@gmail.com>
2020-12-11 15:39:46 -0500

    Update script.js

--- public/script.js
+++ public/script.js
@@ -143,12 +143,4 @@ searchInput.addEventListener('change', displayMatches);
 searchInput.addEventListener('keyup', displayMatches);
 
 
-/*Map*/
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'pk.eyJ1IjoiaGV5ZGF5MyIsImEiOiJja2lrcG81NnAwYzg0MzBxbWl0NmR2ZmEzIn0.AnE-p4rTtbrvqQuK3yn-gA'
-}).addTo(map);
+
